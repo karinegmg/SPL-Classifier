76cddd728407820b247952465231f815235509b3
karinegmg <kgmg@cin.ufpe.br>
2024-01-29 17:28:38 -0300

    refactor: :recycle: refactor function to get features' list

--- features.py
+++ features.py
@@ -1,27 +1,35 @@
-from pydriller import RepositoryMining, GitRepository
-import datetime
-import re
+from pydriller import RepositoryMining
+import os
+from dotenv import load_dotenv
 
-def getLinuxF():
-    featuresL = []
-    featLinux = open('featuresLinuxTeste.csv', 'r')
-    #testeString = ''
+load_dotenv()
+repositoryPath = os.getenv("REPOSITORY_PATH")
+
+def getFeaturesList():
+    featuresList = open('features.csv', 'a')
+    features = []
+    for commit in RepositoryMining(repositoryPath).traverse_commits():
+    
+        for modification in commit.modifications:
+            
+            if('Kconfig' in modification.filename):
+                listDiff = modification.diff.split('\n')
+
+                for line in listDiff:
+                    if('+config ' in line or '+ config' in line):
+                        featureName = line[8:]
+                    if(featureName not in features):
+                        featuresList.write('{}\n'.format(featureName))
+                        features.append(featureName)
+    featuresList.close()
+    return features
+
+def getLinuxFeatures():
+    features = []
+    featLinux = open('featuresLinux.csv', 'r')
 
     for f in featLinux:
-        #print(f.strip())
-        testeString = '{}{}'.format('CONFIG_',f.strip())
-        #print(testeString)
-        featuresL.append(testeString)
+        featureName = '{}{}'.format('CONFIG_',f.strip())
+        features.append(featureName)
     
-    return featuresL
-
-def getFeatures():
-    featuresL = []
-    fileName = 'features_soletta.csv'
-    featuresFile = open(fileName, 'r')
-    for f in featuresFile:
-        #print(f.strip())
-        featName = f.strip()
-        #print(featName)
-        featuresL.append(featName)
-    return featuresL
+    return features
